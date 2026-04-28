87c0585553b2ab6f2d93f8b5213c2aac6ba45f40
basedBug <thebiggestjuw@gmail.com>
2026-04-28 01:06:25 -0700

    feat: add task creation at startup

---
+++ src/tasks/tasks.cpp
@@ -0,0 +1,62 @@
+#include "tasks/tasks.h"
+
+TaskHandle_t xSensorManagerTaskHandle = NULL;
+TaskHandle_t xPidManagerTaskHandle = NULL;
+TaskHandle_t xSysStateManagerTaskHandle = NULL;
+TaskHandle_t xCommsManagerTaskHandle = NULL;
+TaskHandle_t xSysMonitorTaskHandle = NULL;
+
+void createTasks()
+{
+    xTaskCreatePinnedToCore(
+		sensorManagerTask,			// Task function to be called
+		"sensorManager",			// Task name (for debug)
+		12000,						// Stack size (bytes)
+		NULL,						// Task parameters
+		3,							// Priority
+		&xSensorManagerTaskHandle,	// Task handle
+		0							// Core to run on
+	);
+
+	xTaskCreatePinnedToCore(
+		pidManagerTask,			// Task function to be called
+		"pidManager",			// Task name (for debug)
+		10000,					// Stack size (bytes)
+		NULL,					// Task parameters
+		4,						// Priority
+		&xPidManagerTaskHandle,	// Task handle
+		1						// Core to run on
+	);
+
+	xTaskCreatePinnedToCore(
+		sysStateManagerTask,			// Task function to be called
+		"sysStateManager",				// Task name (for debug)
+		8000,							// Stack size (bytes)
+		NULL,							// Task parameters
+		3,								// Priority
+		&xSysStateManagerTaskHandle,	// Task handle
+		1								// Core to run on
+	);
+
+	xTaskCreatePinnedToCore(
+		commsManagerTask,			// Task function to be called
+		"commsManager",				// Task name (for debug)
+		8000,						// Stack size (bytes)
+		NULL,						// Task parameters
+		3,							// Priority
+		&xCommsManagerTaskHandle,	// Task handle
+		1							// Core to run on
+	);
+
+	/*
+	xTaskCreatePinnedToCore(
+		sysMonitorTask,			// Task function to be called
+		"sysMonitor",			// Task name (for debug)
+		7000,					// Stack size (bytes)
+		NULL,					// Task parameters
+		2,						// Priority
+		&sysMonitorTaskHandle,	// Task handle
+		1						// Core to run on
+	);
+	*/
+}
---
+++ src/tasks/tasks.h
@@ -0,0 +1,22 @@
+#pragma once
+
+#include <freertos/FreeRTOS.h>
+#include <freertos/task.h>
+
+#include "sensorManager/sensorManager.h"
+#include "pidManager/pidManager.h"
+#include "sysStateManager/sysStateManager.h"
+#include "commsManager/commsManager.h"
+
+// Sensor handler
+// Thermodynamics/System state control (actuators act in here?)
+// Actuator manager
+// Data handler/ Comms handler
+
+extern TaskHandle_t xSensorManagerTaskHandle;
+extern TaskHandle_t xPidManagerTaskHandle;
+extern TaskHandle_t xSysStateManagerTaskHandle;
+extern TaskHandle_t xCommsManagerTaskHandle;
+extern TaskHandle_t xSysMonitorTaskHandle;
+
+void createTasks();
